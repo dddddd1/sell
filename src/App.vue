4f196659fa59a2d9112916514f67c2c507cb4b55
temple <970450647@qq.com>
2018-02-02 18:59:41 +0800

    路由的配置

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <Header></Header> 
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <!-- <a v-link="{path:'/goods'}">商品</a> -->
+         <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+         <router-link to="/ratings">评论</router-link>  
+        </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link> 
+      </div>
+    </div>
+     <router-view/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Header from './components/header/header.vue'
+import axios from 'axios'
+const ERR_OK= 0;
+
+// Vue.use(axios)
+Vue.prototype.$http = axios
+
+export default {
+  name: 'App',
+  components:{
+    Header
+  },
+  created(){
+    axios.get('/api/seller')
+      .then(function (response) {
+        // response = response.json();
+        /* if (response.errno === ERR_OK){
+          this.seller = response.data;
+          console.log(this.seller);
+        } */
+        console.log(JSON.stringify(response.data.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  data() {
+    return {
+      seller:{}
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "./common/stylus/mixin.styl"
+#app 
+  .tab
+    display flex
+    width 100%
+    height 40px
+    line-height 40px
+    border-1px  rgb(7,17,27)
+    .tab-item
+      flex:1
+      text-align center
+      & a
+        display :block
+        font-size 14px
+        color rgb(77,85,93) 
+        &.active
+          color rgb(240,20,20)
+</style>
+
+
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import header from '@/components/header/header'
+import goods from '@/components/goods/goods'
+import ratings from '@/components/ratings/ratings'
+import seller from '@/components/seller/seller'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    //地址为空时跳转(重定向)goods页面
+    {
+      path: '',   
+      redirect: '/goods'
+    },
+    //头部公共
+    {
+      path: '/header',
+      component: header
+    },
+    //商品页面
+    {
+      path: '/goods',
+      component: goods
+    },
+    //评价页面
+    {
+      path: '/ratings',
+      component: ratings
+    },
+    //商家页面
+    {
+      path: '/seller',
+      component: seller
+    }
+  ],
+  linkExactActiveClass:'active'
+})
+
+
+
